0bb8acaff16c83deace1ee54669f089c18708a8b
dudistData <47510081+dudistData@users.noreply.github.com>
2019-06-29 19:10:08 -0500

    Add files via upload
    the hello world of OOP

---
+++ simple_class_OOP_example.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 29 18:45:58 2019
+
+@author: wilki
+"""
+
+class Point:
+ pass
+p1 = Point()
+p2 = Point()
+p1.x = 5
+p1.y = 4
+p2.x = 3
+p2.y = 6
+print(p1.x, p1.y)
+print(p2.x, p2.y)
